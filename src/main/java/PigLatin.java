bf227d6090ad01fba588f94fee15a0d04dc5c194
Jay Y <alforoanjay@gmail.com>
2023-03-03 04:39:31 +0000

    Completed Pig Latin Lab

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class PigLatin {
     /**
      * Pig latin works as follows: every word has its first character moved to the end, and 'ay' is added to the end of the
@@ -17,6 +17,19 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String[] letters = in.split(" ");
+        StringBuilder result = new StringBuilder();
+        for(String letter : letters){
+            String firstLetter = letter.substring(0,1);
+            String restOfLetters = letter.substring(1);
+            String total = restOfLetters + firstLetter + "ay";
+            result.append(total);
+        }
+        
+        
+
+        
+        
+        return result.toString().trim();
     }
 }
